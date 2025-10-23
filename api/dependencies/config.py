0981dedca68609beea7cb6d0aa1305855c748de0
brad harper <bharpe14@uncc.edu>
2025-10-23 14:39:39 -0400

    .gitignore test

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "9614"
+    db_password = "your_password_here"
     app_host = "localhost"
     app_port = 8000
