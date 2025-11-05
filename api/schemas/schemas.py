f2abb328d25890ff35fa3d6c1804cbd76cac64bf
Brad Harper <bharpe14@uncc.edu>
2025-11-04 20:35:58 -0500

    feat: added customer to schemas.py

--- api/schemas/schemas.py
+++ api/schemas/schemas.py
@@ -4,10 +4,27 @@ from typing import List, Optional
 from enum import Enum
 
 
-# ---------------- ENUM ----------------
+# ENUM
 class OrderStatus(str, Enum):
     PENDING = "Pending"
     PREPARING = "Preparing"
     OUT_FOR_DELIVERY = "Out for Delivery"
     COMPLETED = "Completed"
     CANCELLED = "Cancelled"
+
+# CUSTOMER
+class CustomerBase(BaseModel):
+    name: str
+    phone: str
+    address: str
+
+
+class CustomerCreate(CustomerBase):
+    pass
+
+
+class CustomerRead(CustomerBase):
+    id: int
+
+    class Config:
+        orm_mode = True
