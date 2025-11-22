7a5954464feef4650baef51c3f4d77892274d9e4
Josias Kanyinda <jkkanyinda@gmail.com>
2025-11-21 19:58:29 -0500

    feat: added menu items CRUD operations

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,6 +1,6 @@
 from . import (
   #  customers,
-  #  menu_items,
+    menu_items,
     orders,
     order_details,
   #  payments,
@@ -12,7 +12,7 @@ from . import (
 
 def load_routes(app):
   #  app.include_router(customers.router)
-  #  app.include_router(menu_items.router)
+    app.include_router(menu_items.router)
     app.include_router(orders.router)
     app.include_router(order_details.router)
   #  app.include_router(payments.router)
