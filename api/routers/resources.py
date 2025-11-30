eb907151012db1d97396b405bc335feb95cef34e
Brad Harper <bharpe14@uncc.edu>
2025-11-29 21:08:35 -0500

    feat(routers): changed resources internal tags and hooks to say 'resources' instead of 'ingredients'

--- api/routers/resources.py
+++ api/routers/resources.py
@@ -1,36 +1,37 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from ..controllers import resources as controller
-from ..schemas import resources as schema
-from ..dependencies.database import get_db
+from api.controllers import resources as controller
+from api.schemas import resources as schema
+from api.dependencies.database import get_db
 
 router = APIRouter(
-    tags=["Ingredients"],
-    prefix="/ingredients",
+    tags=["Resources"],
+    prefix="/resources",
 )
 
 
 @router.post("/", response_model=schema.ResourceRead, status_code=status.HTTP_201_CREATED)
-def create_ingredient(request: schema.ResourceCreate, db: Session = Depends(get_db)):
+def create_resource(request: schema.ResourceCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
 
 
 @router.get("/", response_model=list[schema.ResourceRead])
-def read_all_ingredients(db: Session = Depends(get_db)):
+def read_all_resources(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
 
 @router.get("/{resource_id}", response_model=schema.ResourceRead)
-def read_one_ingredient(resource_id: int, db: Session = Depends(get_db)):
+def read_one_resource(resource_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db=db, resource_id=resource_id)
 
 
 @router.put("/{resource_id}", response_model=schema.ResourceRead)
-def update_ingredient(resource_id: int, request: schema.ResourceCreate, db: Session = Depends(get_db)):
+def update_resource(resource_id: int, request: schema.ResourceCreate, db: Session = Depends(get_db)):
     return controller.update(db=db, request=request, resource_id=resource_id)
 
 
 @router.delete("/{resource_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_ingredient(resource_id: int, db: Session = Depends(get_db)):
+def delete_resource(resource_id: int, db: Session = Depends(get_db)):
     return controller.delete(db=db, resource_id=resource_id)
+
