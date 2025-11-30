7a89e9204e6ce5f3b5294c8645fe75f6caea5976
Brad Harper <bharpe14@uncc.edu>
2025-11-29 21:00:16 -0500

    feat(routers): updated index.py to include customers and payments'

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,21 +1,21 @@
 from . import (
-  #  customers,
+    customers,
     menu_items,
     orders,
     order_details,
     reviews,
-  #  payments,
+    payments,
     promotions,
     recipes,
     resources,
 )
 
 def load_routes(app):
-  #  app.include_router(customers.router)
+    app.include_router(customers.router)
     app.include_router(menu_items.router)
     app.include_router(orders.router)
     app.include_router(order_details.router)
-  #  app.include_router(payments.router)
+    app.include_router(payments.router)
     app.include_router(promotions.router)
     app.include_router(recipes.router)
     app.include_router(resources.router)
